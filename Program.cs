3ec96c2d88b3f1ed996cd5d5ac2bd37ef2b7bea8
botea <botea@IULIA>
2022-11-30 13:04:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problema21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random sol = new Random();
+            int solutie = sol.Next(1024);
+            int n;
+            do
+            {
+                Console.Write("Dati un numar intre 1 si 1024: ");
+                n = int.Parse(Console.ReadLine());
+                if (n < solutie)
+                    Console.WriteLine("Numarul este prea mic!");
+                else if (n == solutie)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Bravo! Ai ghicit!");
+                }
+                else
+                    Console.WriteLine("Numarul  prea mare!");
+            }
+            while (n != solutie);
+            Console.ReadKey();
+        }
+    }
+}
